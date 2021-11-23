e56737dba1036460a20304741b3e32e447689b97
Brenda Long <brenda@studio6thave.com>
2021-11-23 09:41:24 -0600

    sample of sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Button } from 'reactstrap';
 import './styles/custom.scss';
 
 function App() {
   return (
     <div className="App">
       <h1 class="bg-dark">Pretty Groovy Jumbo Jet</h1>
+      <button type="button" class="btn btn-primary">Primary</button>
+
+      <div>
+  <Button
+    color="primary"
+  >
+    Primary
+  </Button>
+</div>
     </div>
   );
 }
