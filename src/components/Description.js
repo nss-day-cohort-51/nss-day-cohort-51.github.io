dc35290bb00488a9fdbcffa7dac9f70558a52ce3
tgann11 <morganne1170@gmail.com>
2021-11-23 13:58:16 -0600

    mm-feature-description-8 commiting to mrerge

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,14 +1,20 @@
 //Purpose: Creates and displays the Description component
-
 import React from "react";
 import "../styles/_colors.scss";
 import "../styles/custom.scss";
+import logo from "../images/logo.png"
+import "../index.css"
+import "../styles/description.scss"
+
+
 
 export const Description = () => {
   return (
     <>
-      <div id="description" className="description">
-        DESCRIPTION
+    <div className="logo"><img className="logo__img" src={logo} alt="C51" /></div>
+      <div id="description" className="description-section__about">
+        <h1>THE DEVELOPERS ARE OUT THERE!</h1>
+        <p>Our Cohort 51 was completely remote. We are a mix of both Full-Stack and UI/UX Front-End Developers. See our Tech Stack here.</p>
       </div>
     </>
   );
