a3624c8a4eec542a531e908ecee4f8814d39deeb
Jake Lashenik <jmlashen@gmail.com>
2021-11-24 12:32:38 -0600

    jl-feature-developercard-developerlist-16-11 created dev card and dev list displaying a dev card

--- src/components/DevList.js
+++ src/components/DevList.js
@@ -3,12 +3,30 @@
 import React from "react";
 import { DevCard } from "../components/DevCard";
 import "../styles/custom.scss";
+import { useEffect, useState } from "react";
+import { getAllDevs } from "./APIManager";
 
 export const DevList = () => {
+
+  const [allDevs, setAllDevs] = useState([]);
+  const getDevs = () => {
+      getAllDevs().then(allDevsFromAPI => {
+
+          console.log("alldevfrom api are", allDevsFromAPI);
+          setAllDevs(allDevsFromAPI)
+
+      });
+  };
+
+  useEffect(() => {
+    getDevs();
+}, []);
+
+
   return (
     <>
       <div id="devList" className="dev-list">
-        <DevCard />
+      {allDevs.map(obj =><DevCard key={obj.id} obj={obj}/>)}
       </div>
     </>
   );
