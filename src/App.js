154d8d5af9f71e2546143f4839227e1e654ac0bd
Susie Stanley <susiestanley00@gmail.com>
2021-11-29 11:40:59 -0600

    updated scrolling text location

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { NavBar } from "./components/NavBar";
 import { Description } from "./components/Description";
 import { DevList } from "./components/DevList";
-import { ScrollingText } from "./components/TextAnimation";
 import { TechStack } from "./components/TechStack";
 import { ThanksLocation } from "./components/ThanksLocation";
 import { Footer } from "./components/Footer";
@@ -24,7 +23,6 @@ export const App = () => {
     <div className="page">
       <NavBar />
       <Description />
-      <ScrollingText />
       <DevList allDevs={allDevs} />
       <TechStack allTechStack={allTechStack} />
       <ThanksLocation />
--- src/components/DevList.js
+++ src/components/DevList.js
@@ -1,14 +1,16 @@
 //Purpose: Creates and displays the Developer List component
 
 import React from "react";
+import { ScrollingText } from "./components/TextAnimation";
 import { DevCard } from "../components/DevCard";
 import "../styles/custom.scss";
 
 export const DevList = ({ allDevs }) => {
-
   return (
     <>
       <div id="devList" className="dev-list">
+        <ScrollingText />
+
         {allDevs.map((obj) => (
           <DevCard key={obj.id} obj={obj} />
         ))}
