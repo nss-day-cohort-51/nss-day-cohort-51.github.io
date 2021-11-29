f63b3d8c8894cf4542765ec2613c8f9ca598514a
Susie Stanley <susiestanley00@gmail.com>
2021-11-29 11:29:38 -0600

    added typing and scrolling text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavBar } from "./components/NavBar";
 import { Description } from "./components/Description";
 import { DevList } from "./components/DevList";
-import { ScrollingText, TypingText1 } from "./components/TextAnimation";
+import { ScrollingText } from "./components/TextAnimation";
 import { TechStack } from "./components/TechStack";
 import { ThanksLocation } from "./components/ThanksLocation";
 import { Footer } from "./components/Footer";
@@ -12,7 +12,6 @@ export const App = () => {
   return (
     <div className="page">
       <NavBar />
-      <TypingText1 />
       <Description />
       <ScrollingText />
       <DevList />
