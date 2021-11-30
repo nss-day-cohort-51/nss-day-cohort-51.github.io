9fdac7f654026d4a014a2dc08e4bb2e2045aebc1
Susie Stanley <susiestanley00@gmail.com>
2021-11-29 22:48:50 -0600

    styled modal

--- src/components/MediaButtons.js
+++ src/components/MediaButtons.js
@@ -16,85 +16,101 @@ export const CapBtn = ({ obj }) => {
       <div className="cap-btn" onClick={handleShow}>
         CAPSTONE
       </div>
-      <div className="modal">
-        <Modal size="xl" show={show} onHide={handleClose} centered>
-          <Modal.Header closeButton>
-            <Modal.Title>
-              <div className="modal-name">{obj?.name}</div>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <div className="modal-wrapper">
-              <div className="modal-list__left">
-                <div className="modal-pic">
-                  {obj?.pic ? (
-                    <img
-                      src={require(`../images/${obj?.pic}`).default}
-                      alt={obj?.name}
-                      className=""
-                    />
-                  ) : (
-                    <img
-                      src={require(`../images/default.png`).default}
-                      alt="default-user"
-                      className=""
-                    />
-                  )}
+      {/* <div className="modal"> */}
+      <Modal size="xl" show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <div className="modal-name">{obj?.name}</div>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="modal-wrapper">
+            <div className="modal-list__left">
+              <div className="modal-pic">
+                {obj?.pic ? (
+                  <img
+                    src={require(`../images/${obj?.pic}`).default}
+                    alt={obj?.name}
+                    className="modal-img"
+                  />
+                ) : (
+                  <img
+                    src={require(`../images/default.png`).default}
+                    alt="default-user"
+                    className=""
+                  />
+                )}
+              </div>
+              <div className="modal-devtitle">
+                {obj?.title}
+                <div className="modal-icons">
+                  <MediaIcons key={obj.id} obj={obj} />
                 </div>
-                <div className="modal-title">
-                  {obj?.title}
-                  <div className="modal-icons">
-                    <MediaIcons key={obj.id} obj={obj} />
+
+                <div className="modal-intfact-wrapper">
+                  <div className="modal-interests">
+                    <strong>Industry Interest: </strong>
+                    {obj?.interests}
+                  </div>
+                  <div className="modal-fact">
+                    <strong>Fun Fact: </strong>
+                    {obj?.fact}
+                  </div>
+                </div>
+
+                <div className="modal-pod">
+                  <div className="pod-title">Podcast Interview</div>
+                  <div className="pod-wrapper">
+                    <iframe
+                      title="Podcast Player"
+                      width="80%"
+                      height="20"
+                      scrolling="no"
+                      frameborder="no"
+                      src="https://w.soundcloud.com/player/?url=https://soundcloud.com/discord96/materia-primoris-the-x-files-theme-full-illuminati-song&color=#82bad9&color=%23ff5500&inverse=true&auto_play=false&show_user=true"
+                    ></iframe>
                   </div>
                 </div>
               </div>
-              <div className="modal-list__right">
-                <div className="modal-interests">{obj?.interests}</div>
-                <div className="dev-card__fact">{obj?.fact}</div>
-                <div className="capstone-wrapper">
-                  <div className="cap1">
-                    <iframe
-                      width="360"
-                      height="202"
-                      src={obj?.capLink}
-                      title="YouTube video player"
-                      frameborder="0"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                      allowfullscreen
-                    ></iframe>
-                  </div>
-                  <div className="cap2">
-                    <iframe
-                      width="360"
-                      height="202"
-                      src={obj?.cap2Link}
-                      title="YouTube video player"
-                      frameborder="0"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                      allowfullscreen
-                    ></iframe>
-                  </div>
-                </div>
-                <div className="modal-pod">
+            </div>
+            <div className="modal-list__right">
+              <h4>Capstone Video Demos</h4>
+              <div className="capstone-wrapper">
+                <div className="cap1">
+                  <h5>{obj?.capName}</h5>
                   <iframe
-                    title="Podcast Player"
-                    width="80%"
-                    height="20"
-                    scrolling="no"
-                    frameborder="no"
-                    src="https://w.soundcloud.com/player/?url=https://soundcloud.com/discord96/materia-primoris-the-x-files-theme-full-illuminati-song&color=#82bad9&color=%23ff5500&inverse=true&auto_play=false&show_user=true"
+                    width="360"
+                    height="202"
+                    src={obj?.capLink}
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                  ></iframe>
+                </div>
+                <div className="cap2">
+                  <h5>{obj?.cap2Name}</h5>
+                  <iframe
+                    width="360"
+                    height="202"
+                    src={obj?.cap2Link}
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
                   ></iframe>
                 </div>
               </div>
             </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      {/* </div> */}
     </>
   );
 };
