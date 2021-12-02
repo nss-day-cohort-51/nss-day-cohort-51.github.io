411c65922a24bb5430d8ad0a0601ce07a9555c8c
Michael Wright <info@saymoremultimedia.com>
2021-12-02 13:56:42 -0600

    mw-fix-TechStack-reposition-lists-75: updated Texh Stack Styling; added more line-height to texh list items; reaaranged the list blocks

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -17,10 +17,13 @@ export const TechStack = ({ allTechStack }) => {
   return (
     <>
       <section id="techStack" className="tech-stack background-overlay">
+        <div className="background-overlay"></div>
         <h2>Tech Stack</h2>
         <section id="combined-stack__block">
           <section id="combined-stack-list__block" className="list__block">
             <h4 className="section__title">We All Learned the Following</h4>
+            <p className="combined__description">Our hybrid cohort developed  skills and resources.</p>
+
             <ul
               id="combined-stack__list"
               className="no-bullet__list stack__list"
@@ -35,6 +38,7 @@ export const TechStack = ({ allTechStack }) => {
             <section id="front-end-stack__block" className="list__block">
               {" "}
               <h4 className="section__title">UI/UX FRONT-END</h4>
+              <p className="front-end__description">Front-End Devs focused on streamlining web application design and user experience.</p>
               <ul
                 id="frontend-stack__list"
                 className="no-bullet__list stack__list"
@@ -47,6 +51,7 @@ export const TechStack = ({ allTechStack }) => {
             <section id="full-stack__block" className="list__block">
               {" "}
               <h4 className="section__title">FULL STACK</h4>
+              <p className="full-stack__description">Full-Stack Devs focused on the backend and functionality of a web application.</p>
               <ul id="full-stack__list" className="no-bullet__list stack__list">
                 {allTheFullStack.map((stackItem) => (
                   <TechStackButton key={stackItem.id} stackItem={stackItem} />
