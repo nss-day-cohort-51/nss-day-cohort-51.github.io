e194eaec6cc3e508e647de70fb4110f4932c3245
Jake Lashenik <jmlashen@gmail.com>
2021-12-03 16:04:17 -0600

    added new logo and needs positioning with description section

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,6 +1,6 @@
 //Purpose: Creates and displays the Description component
 import React from "react";
-import logo from "../images/logo.png";
+import logo2 from "../images/logo2.svg";
 import backgroundImage from "../images/description.svg";
 import { DemoDayButton } from "../components/DemoDayButton";
 
@@ -9,15 +9,17 @@ export const Description = () => {
     <>
 
       <div id="description" className="description">
-        <div className="logo"><img className="logo-img" src={logo} alt="C51" /></div>
+        <div className="logo"><img className="logo-img" src={logo2} alt="C51" /></div>
         <div className="description-section__about"
-        style={{ backgroundImage: `url(${backgroundImage})`, backgroundRepeat: `no-repeat`, backgroundSize: `auto 100%`, backgroundPosition: `right bottom` }} >
-         <h1>Nashville Software School Presents Cohort 51!</h1>
-          <h2>THE DEVELOPERS ARE OUT THERE!</h2>
-          <p>We are <strong>Nashville Software School's</strong> Cohort 51! Our Cohort is a little different. We learned front end foundations together, and then split into two paths: UI/UX Design and Full Stack. See our <a href="http://localhost:3000/#techStack" rel="techstack">
-            Tech Stack </a></p>
-          <p>Abduct a developer at our virtual demo day on <strong>February 11, 2022</strong></p>
+          style={{ backgroundImage: `url(${backgroundImage})`, backgroundRepeat: `no-repeat`, backgroundSize: `auto 100%`, backgroundPosition: `right bottom` }} >
+          <div className="about-wrapper">
+            <h1>Nashville Software School Presents Cohort 51!</h1>
+            <h2>THE DEVELOPERS ARE OUT THERE!</h2>
+            <p>We are <strong>Nashville Software School's</strong> Cohort 51! Our Cohort is a little different. We learned front end foundations together, and then split into two paths: UI/UX Design and Full Stack. See our <a href="http://localhost:3000/#techStack" rel="techstack">
+              Tech Stack </a></p>
+            <p>Abduct a developer at our virtual demo day on <strong>February 11, 2022</strong></p>
             <DemoDayButton />
+          </div>
         </div>
       </div>
     </>
