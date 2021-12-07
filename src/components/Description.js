c5c4b5589c58260b06ba1a3732714f94b2689839
Susie Stanley <susiestanley00@gmail.com>
2021-12-07 02:32:30 -0600

    ss-feature-hoverlinks-79: made description section mobile responsive using media query

--- src/components/Description.js
+++ src/components/Description.js
@@ -7,18 +7,31 @@ import { DemoDayButton } from "../components/DemoDayButton";
 export const Description = () => {
   return (
     <>
-
       <div id="description" className="description">
-        <div className="logo"><img className="logo-img" src={logo} alt="C51" /></div>
-        <div className="description-section__about"
-        style={{ backgroundImage: `url(${backgroundImage})`, backgroundRepeat: `no-repeat`, backgroundSize: `auto 100%`, backgroundPosition: `right bottom` }} >
-         <h1>Nashville Software School Presents Cohort 51!</h1>
+        <div className="logo">
+          <img className="logo-img" src={logo} alt="C51" />
+        </div>
+        <div className="description-section__about">
+          <h1>Nashville Software School Presents Cohort 51!</h1>
           <h2>THE DEVELOPERS ARE OUT THERE!</h2>
-          <p>We are <strong>Nashville Software School's</strong> Cohort 51! Our Cohort is a little different. We learned front end foundations together, and then split into two paths: <span className="fullstack">Full Stack</span> and <span className="uiux">UI/UX</span> Developers. 
-          See our <a href="https://nss-day-cohort-51.github.io/#techStack" rel="techstack">
-            Tech Stack </a></p>
-          <p>Abduct a developer at our virtual demo day on <strong>February 11, 2022</strong></p>
-            <DemoDayButton />
+          <p>
+            We are <strong>Nashville Software School's</strong> Cohort 51! Our
+            Cohort is a little different. We learned front end foundations
+            together, and then split into two paths:{" "}
+            <span className="fullstack">Full Stack</span> and{" "}
+            <span className="uiux">UI/UX</span> Developers. See our{" "}
+            <a
+              href="https://nss-day-cohort-51.github.io/#techStack"
+              rel="techstack"
+            >
+              Tech Stack{" "}
+            </a>
+          </p>
+          <p>
+            Abduct a developer at our virtual demo day on{" "}
+            <strong>February 11, 2022</strong>
+          </p>
+          <DemoDayButton />
         </div>
       </div>
     </>
