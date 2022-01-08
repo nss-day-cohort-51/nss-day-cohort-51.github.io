12fda6e66514a44e48d4d03d005abbc78c89b15a
tgann11 <morganne1170@gmail.com>
2022-01-08 13:03:00 -0600

    merging to main

---
+++ src/components/Buttons.js
@@ -0,0 +1,17 @@
+//Purpose: Creates and displays the Demo Day button component
+//https://www.nashss.com/demoday - will not redirect to our eventbrite page until January 12th. We’ll start promoting it to employers the week of January 17th
+
+import React from "react";
+import "../styles/custom.scss";
+
+export const DemoDayButton = () => {
+  return (
+    <>
+      <a href="https://www.nashss.com/demoday" target="_blank" rel="noreferrer">
+        <div className="demo-day-btn">
+          <div className="btn-item">Join Us For Demo Day</div>
+        </div>
+      </a>
+    </>
+  );
+};
