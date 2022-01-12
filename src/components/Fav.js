448045115845b66c0df2f70b514afa36a382cdb3
Michael Wright <info@saymoremultimedia.com>
2022-01-12 10:31:18 -0600

    mw-fix-favorite-Developer-DevCard-sections-146 Removed checkForFav function from the return of Fav.js.

--- src/components/Fav.js
+++ src/components/Fav.js
@@ -5,44 +5,41 @@ Place this component in the DevCard and pass it the devId
 
 import React, { useEffect } from "react";
 import { useState } from "react";
-import { checkForFav, getFavs, updateFavs } from './FavsManager';
+import { checkForFav, getFavs, updateFavs } from "./FavsManager";
 // import { FaRegHeart } from 'react-icons';
 
 export const Fav = ({ devId }) => {
-	const [isFavorite, setisFavorite] = useState(false)
+  const [isFavorite, setisFavorite] = useState(false);
 
-	//update localStorage
-	const handleFavChange = () => {
-		const storageToChange = getFavs();
-		const foundFavorite = storageToChange.findIndex(oneFav => oneFav === devId);
+  //update localStorage
+  const handleFavChange = () => {
+    const storageToChange = getFavs();
+    const foundFavorite = storageToChange.findIndex(
+      (oneFav) => oneFav === devId
+    );
 
-		if (foundFavorite !== -1) {
-			//remove it
-			storageToChange.splice(foundFavorite, 1);
-			setisFavorite(false)
-		} else {
-			//add it
-			storageToChange.push(devId)
-			setisFavorite(true)
-		}
-		updateFavs(storageToChange);
-	};
+    if (foundFavorite !== -1) {
+      //remove it
+      storageToChange.splice(foundFavorite, 1);
+      setisFavorite(false);
+    } else {
+      //add it
+      storageToChange.push(devId);
+      setisFavorite(true);
+    }
+    updateFavs(storageToChange);
+  };
 
-	useEffect(() => {
-		setisFavorite(checkForFav(devId));
-	}, [isFavorite])
+  useEffect(() => {
+    setisFavorite(checkForFav(devId));
+  }, [isFavorite]);
 
-
-	return (
-		<>
-		
-		{checkForFav(devId)}
-		<span onClick={handleFavChange}>
-			<input type="checkbox" checked={isFavorite ? true : false} />
-			<span></span>
-		</span>
-			
-		</>
-	)
-}
-
+  return (
+    <>
+      <span onClick={handleFavChange}>
+        <input type="checkbox" checked={isFavorite ? true : false} />
+        <span></span>
+      </span>
+    </>
+  );
+};
